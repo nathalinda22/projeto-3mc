716b292c74b1fa4044c26847cc1b8e16be3eafd9
nathalinda22 <129405010+nathalinda22@users.noreply.github.com>
2024-05-17 08:01:32 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# projeto-3mc
+# alura
--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-
-        for (let j = 0; j < botoes.length; j++) {
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        
+        for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-
+        
         botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
+        botoes[i].classList.add("ativo");
     }
 }
 
@@ -20,7 +20,7 @@ const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 
 function calculaTempo(tempoObjetivo) {
@@ -39,18 +39,20 @@ function calculaTempo(tempoObjetivo) {
     } else {
         return "Prazo Finalizado";
     }
+    
 }
 
 function atualizaCronometro(){
-       
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
-
+    
 function comecaCronometro(){
-    atualizaCronometro();
-    setInterval(atualizaCronometro,1000);
-}
-
-//comecaCronometro();
+        atualizaCronometro();
+        setInterval(atualizaCronometro, 1000);
+ }
+    
+comecaCronometro();
+    
+    
